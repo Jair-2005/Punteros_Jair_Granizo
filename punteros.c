13c249f04f9e8438866ff8782957554fdd579cfe
Jair Granizo <steven.granizo@udla.edu.ec>
2026-05-06 15:11:18 -0500

    Agregar float

--- punteros.c
+++ punteros.c
@@ -7,5 +7,13 @@ int *x; //puntero
 x=&num; // almacena la direccion de num
 
 printf ("%d %p",num, x);
+
+
+float pi = 3.1416, decimal;
+float *ptrf; // definicion de puntero
+
+ptrf = &pi;
+
+printf ("%f %p",pi, ptrf);
     return 0;
 }
