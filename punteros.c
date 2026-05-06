690b095d46fc9dedf823d5a48b27b50d4df3fd1c
Jair Granizo <steven.granizo@udla.edu.ec>
2026-05-06 15:20:32 -0500

    Tercer commit

--- punteros.c
+++ punteros.c
@@ -5,15 +5,17 @@ int num=150, a;
 int *x; //puntero
 
 x=&num; // almacena la direccion de num
+a=*x;
 
-printf ("%d %p",num, x);
+
+printf ("%d %d %p",num, a, x);
 
 
 float pi = 3.1416, decimal;
 float *ptrf; // definicion de puntero
 
 ptrf = &pi;
-
-printf ("%f %p",pi, ptrf);
+decimal = *ptrf;
+printf ("%f %f %p",pi, decimal, ptrf);
     return 0;
 }
